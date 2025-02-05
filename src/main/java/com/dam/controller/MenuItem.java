a0e63df96f77a4abf6f0dbf1734fd6c9f6ea3537
Jinghao <jlin031@g.educaand.es>
2025-02-05 18:33:36 +0100

    menu item

---
+++ src/main/java/com/dam/controller/MenuItem.java
@@ -0,0 +1,40 @@
+package com.dam.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MenuItem {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnRemove;
+
+    @FXML
+    private Button btnRun;
+
+    @FXML
+    private Label labelDescription;
+
+    @FXML
+    private Label labelTitle;
+
+    @FXML
+    void onDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRemove(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRun(ActionEvent event) {
+
+    }
+
+}
