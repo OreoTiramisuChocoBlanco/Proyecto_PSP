faecbf74b98d5efc47e35daba425a8a86ef9e261
AkrexNar <jlin031@g.educaand.es>
2025-02-05 18:10:37 +0100

    added fxml methods

---
+++ src/main/java/com/dam/controller/Main.java
@@ -0,0 +1,49 @@
+package com.dam.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class Main {
+
+    @FXML
+    private MenuItem btnImport;
+
+    @FXML
+    private MenuItem btnNew;
+
+    @FXML
+    private MenuItem btnOpen;
+
+    @FXML
+    private MenuItem btnQuit;
+
+    @FXML
+    private MenuItem btnSave;
+
+    @FXML
+    void openFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openImport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openNew(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quit(ActionEvent event) {
+
+    }
+
+}
